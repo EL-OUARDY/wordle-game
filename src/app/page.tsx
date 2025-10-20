a8454c72caa0799233b10a28b8eff27f69f63d86
EL-OUARDY <ouadie.elouardy@gmail.com>
2025-10-20 14:38:22 +0100

    Increase maximum width of main content area to 520px

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="page-wrapper font-body flex min-h-screen flex-col select-none">
       <Header />
-      <main className="mx-auto flex w-full max-w-[500px] flex-1 flex-col items-center justify-center">
+      <main className="mx-auto flex w-full max-w-[520px] flex-1 flex-col items-center justify-center">
         <Wordle />
       </main>
       <Footer />
