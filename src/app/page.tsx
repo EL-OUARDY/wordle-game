eeb94e8224ba24808adb1bb0f94a1ed04e95e2c4
EL-OUARDY <ouadie.elouardy@gmail.com>
2025-10-22 23:01:55 +0100

    Remove debug component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import StoreDebug from "@/components/StoreDebug";
 import Wordle from "@/components/Wordle";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <div className="page-wrapper font-body flex min-h-screen flex-col select-none">
-      <StoreDebug />
       <Header />
       <main className="mx-auto flex w-full max-w-[520px] flex-1 flex-col items-center justify-center">
         <Wordle />
