ab3f8be0e347c77e91e5920e016cc45433d08274
EL-OUARDY <ouadie.elouardy@gmail.com>
2025-10-26 12:43:06 +0100

    Redirect to home page upon login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,19 @@
+"use client";
 import Login from "@/components/Login";
+import useAuth from "@/hooks/useAuth";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Page() {
+  const { user } = useAuth();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    // Navigates to home page when user logs in
+    if (user) router.push("/");
+  }, [router, user]);
+
   return (
     <main className="mx-auto flex w-full max-w-[520px] flex-1 flex-col items-center justify-center">
       <Login className="max-w-md" />
